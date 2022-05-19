7322aaff8f8d49517ae163a035ef378bcbecefc0
matheusolri <matheusdeoliveirarib@gmail.com>
2022-05-19 11:09:48 -0300

    https://github.com/matheusolri/Aprendendo-ReactJS.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  //praticando aprendizado de Props
+  const Equipe = (props) => {
+    return (
+      <div>
+         <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
+         <Social instagram={props.instagram}/>
+         <hr/>
+      </div>
+    );
+  }
+  const Sobre = (props) => {
+    return (
+      <div>
+        <h1>Olá sou o(a):{props.nome}</h1>
+        <h2>meu cargo é : {props.cargo}</h2>
+        <h2>minha idade é : {props.idade}</h2>
+        
+      </div>
+    );
+  }
+  const Social = (props)=>{
+    return(
+      <a href={props.instagram}>Instagram</a>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Conheça nossa equipe</h1>
+      <Equipe nome="Matheus" cargo="Desenvolvedor Front" idade="20" instagram="https://www.instagram.com/matheus_olri/"  />
+      <Equipe nome="Anderson" cargo="Desenvolvedor Back" idade="22" instagram="https://www.instagram.com/anderson.campos_/" />
     </div>
   );
 }
-
 export default App;
