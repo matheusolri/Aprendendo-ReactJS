8fcdc2bc6d53fca2159c80e873b387998612d8b2
matheusolri <matheusdeoliveirarib@gmail.com>
2022-07-22 16:20:00 -0300

    trabalhando com ciclo de vida dos componentes

--- src/App.js
+++ src/App.js
@@ -1,47 +1,31 @@
 
 import React, { Component } from "react";
 
-//trabalhando com States
-
 class App extends Component {
-  constructor(props) {
-    super(props);//dando autorização para usar tudo do componente
-    this.state = {
-      contador: 0 //declarando state contador dentro do meu construtor
-    }
-    this.diminuir = this.diminuir.bind(this);// aqui nos usamos o bind para que a função aumentar seja usada e acessada
-    this.aumentar = this.aumentar.bind(this);// aqui nos usamos o/ bind para que a função aumentar seja usada e acessada
-  }
-  //Funçoes de aumentar e diminuir
-  diminuir() {
+  constructor(props){
+    super(props);
+    this.state={
 
-    let estado = this.state; //atribuindo todas minhas states a variavel estado
-    if (estado.contador == 0) {
-      alert('voçe chegou a 0')
-      return;
-    }
-    estado.contador = estado.contador - 1;
-    this.setState(estado);//setando a state com seu novo valor
+      hora: '00:00:00'
+    };
   }
-  aumentar() {
+  componentDidMount(){
+    setInterval(()=>{
+      this.setState({hora:new Date().toLocaleTimeString()})
+  },1000);
+  }//serve pra consumir api extra
 
-    let estado = this.state //atribuindo todas minhas states a variavel estado
-    estado.contador += 1;
-    this.setState(estado);//setando a state com seu novo valor
+  componentDidUpdate(){
+    console.log('atualizou');//pegar atualização em algum estado e fazer alguma ação
   }
-
   render() {
     return (
       <div>
-        <h1>contador</h1>
-        <h3>
-          <button onClick={this.diminuir}>-</button> {/* botão chama a função diminuir */}
-          {this.state.contador}{/* chamando a state contador */}
-          <button onClick={this.aumentar}>+</button> {/* botão chama a função aumentar */}
-        </h3>
+        <h1>
+       {this.state.hora}
+        </h1>
       </div>
     );
   }
 }
-
 export default App;
